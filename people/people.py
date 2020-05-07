5d56d7caa73435689989c3cb8c98ba3b4b8b5ac0
Ivan Hudzynskyi <ivanhudzinskyi2000@gmail.com>
2020-05-08 01:45:07 +0300

    Add Employee class
    Signed-off-by: Ivan Hudzynskyi <ivanhudzinskyi2000@gmail.com>

--- people/people.py
+++ people/people.py
@@ -1,13 +1,23 @@
 from abc import ABC, abstractclassmethod
 
+# class Person(ABC):
 class Person(ABC):
-    def __init__(self, first_name, last_name, age):
+    def __init__(self, first_name, last_name, age, salary):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+        self.salary = salary
 
-    @abstractclassmethod
     def __str__(self):
-        return f"Name: {self.first_name} {self.last_name}\nAge: {self.age}"
+        return f"Name: {self.first_name} {self.last_name}\nAge: {self.age}\nSalary: {self.salary}\n"
 
-# TODO: think of what should be in Employee, Worker, Engineer
+
+class Employee(Person):
+    def __init__(self, first_name, last_name, age, salary, company_name):
+        super().__init__(first_name, last_name, age, salary)
+        self.company_name = company_name
+
+    def __str__(self):
+        res = super().__str__()
+        res += f"Company: {self.company_name}\n"
+        return res
