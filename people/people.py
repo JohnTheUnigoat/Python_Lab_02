e3648ca286d48f6721bff495f9c1ac9d226fadb4
Ivan Hudzynskyi <ivanhudzinskyi2000@gmail.com>
2020-05-09 01:33:34 +0300

    Add email property to people with overloads
    Signed-off-by: Ivan Hudzynskyi <ivanhudzinskyi2000@gmail.com>

--- people/people.py
+++ people/people.py
@@ -7,14 +7,22 @@ class Person(ABC):
         self.age = age
         self.salary = salary
 
+    @property
+    def email(self):
+        return f"{self.first_name.lower()}.{self.last_name.lower()}@mail.com"
+
     def __str__(self):
-        return f"Name: {self.first_name} {self.last_name}\nAge: {self.age}\nSalary: {self.salary}\n"
+        return f"Name: {self.first_name} {self.last_name}\nAge: {self.age}\nEmail: {self.email}\nSalary: {self.salary}\n"
 
 
 class Employee(Person):
     def __init__(self, first_name, last_name, age, salary, company_name):
         super().__init__(first_name, last_name, age, salary)
         self.company_name = company_name
+
+    @property
+    def email(self):
+        return f"{self.first_name.lower()}.{self.last_name.lower()}@companyname.com"
 
     def __str__(self):
         res = super().__str__()
@@ -37,6 +45,10 @@ class Engineer(Person):
     def __init__(self, first_name, last_name, age, salary, speciality):
         super().__init__(first_name, last_name, age, salary)
         self.speciality = speciality
+
+    @property
+    def email(self):
+        return f"{self.first_name.lower()}_{self.last_name.lower()}@mail.com"
 
     def __str__(self):
         res = super().__str__()
